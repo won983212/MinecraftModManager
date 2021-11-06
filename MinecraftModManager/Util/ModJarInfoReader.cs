c525a18332dfabf6ce19da3e9d4e6063e9f59ce8
이현 <won983212@naver.com>
2021-11-07 02:17:30 +0900

    로직 분리

---
+++ MinecraftModManager/Util/ModJarInfoReader.cs
@@ -0,0 +1,128 @@
+﻿using MinecraftModManager.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace MinecraftModManager.Util
+{
+    public class ModJarInfoReader
+    {
+        private string filePath;
+        private ZipArchive archive = null;
+        private readonly ModsTomlParser tomlParser = new ModsTomlParser();
+
+        public Mod ParseModFromJar(string filePath)
+        {
+            Logger.Debug("Open " + filePath);
+            this.filePath = filePath;
+            Mod result = null;
+            using (archive = ZipFile.OpenRead(filePath))
+            {
+                ZipArchiveEntry modsTomlEntry = archive.GetEntry("META-INF/mods.toml");
+                if (modsTomlEntry == null)
+                    return null;
+                using (Stream stream = modsTomlEntry.Open())
+                {
+                    StreamReader streamReader = new StreamReader(stream);
+                    result = ParseModFromTomlStream(streamReader);
+                }
+                FillVersionAndThumbnail(result);
+            }
+            archive = null;
+            return result;
+        }
+
+        private void FillVersionAndThumbnail(Mod mod)
+        {
+            if (mod == null)
+                return;
+            if (mod.LogoPath != null)
+                mod.Thumbnail = CreateLogoImage(mod.LogoPath);
+            if (mod.Version == "${file.jarVersion}")
+                mod.Version = GetJarVersion();
+            if (mod.Version == null)
+                mod.Version = Path.GetFileName(filePath);
+        }
+
+        private BitmapImage CreateLogoImage(string logoFile)
+        {
+            ZipArchiveEntry logoFileEntry = archive.GetEntry(logoFile);
+            if (logoFileEntry == null)
+                return null;
+            BitmapImage image;
+            using (MemoryStream stream = new MemoryStream())
+            {
+                using (Stream imageStream = logoFileEntry.Open())
+                {
+                    imageStream.CopyTo(stream);
+                }
+                image = new BitmapImage();
+                image.BeginInit();
+                image.StreamSource = stream;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.EndInit();
+                image.Freeze();
+            }
+            return image;
+        }
+
+        private string GetJarVersion()
+        {
+            ZipArchiveEntry manifestEntry = archive.GetEntry("META-INF/MANIFEST.MF");
+            if (manifestEntry == null)
+                return null;
+            using (Stream stream = manifestEntry.Open())
+            {
+                StreamReader streamReader = new StreamReader(stream);
+                return ReadManifestVersion(streamReader);
+            }
+        }
+
+        private string ReadManifestVersion(StreamReader streamReader)
+        {
+            string line;
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                line = line.Trim();
+                int separatorIndex = line.IndexOf(':');
+                if (separatorIndex == -1)
+                    continue;
+                string propertyKey = line.Substring(0, separatorIndex).Trim();
+                string propertyValue = line.Substring(separatorIndex + 1).Trim();
+                if (propertyKey == "Implementation-Version")
+                    return propertyValue;
+            }
+            return null;
+        }
+
+        private Mod ParseModFromTomlStream(StreamReader streamReader)
+        {
+            Dictionary<string, string> properties = tomlParser.ParseToml(streamReader);
+            if (!ValidateTomlProperty(properties))
+                return null;
+
+            Mod mod = new Mod();
+            mod.Name = properties["displayName"];
+            mod.Version = properties["version"];
+            if (properties.TryGetValue("description", out string description))
+                mod.Description = description;
+            if (properties.TryGetValue("logoFile", out string logoFile))
+                mod.LogoPath = logoFile;
+            return mod;
+        }
+
+        private bool ValidateTomlProperty(Dictionary<string, string> properties)
+        {
+            if (!properties.ContainsKey("displayName"))
+                return false;
+            if (!properties.ContainsKey("version"))
+                return false;
+            return true;
+        }
+    }
+}
---
+++ MinecraftModManager/Util/ModsTomlParser.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinecraftModManager.Util
+{
+    public class ModsTomlParser
+    {
+        private string line;
+        private Dictionary<string, string> properties;
+        private string continuedStringPropertyName = null;
+
+        public Dictionary<string, string> ParseToml(StreamReader streamReader)
+        {
+            Reset();
+            ReadToNextModsCategory(streamReader);
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                line = line.Trim();
+                if (line.StartsWith("#"))
+                    continue;
+                if (!IsModsCategory())
+                    break;
+                if (AppendContinuedString())
+                    continue;
+                int separatorIndex = line.IndexOf('=');
+                if (separatorIndex == -1)
+                    continue;
+                AddProperty(separatorIndex);
+            }
+            return properties;
+        }
+
+        private void Reset()
+        {
+            line = null;
+            properties = new Dictionary<string, string>();
+            continuedStringPropertyName = null;
+        }
+
+        private bool IsModsCategory()
+        {
+            string categoryName = ExtractCategoryName(line);
+            return categoryName == null || categoryName == "mods";
+        }
+
+        private bool AppendContinuedString()
+        {
+            if (continuedStringPropertyName != null)
+            {
+                string prettyLine = EliminateLiteralMark(line);
+                prettyLine = EliminateColorCode(prettyLine);
+                properties[continuedStringPropertyName] += prettyLine;
+                if (ContainsMultiLineEnd(line))
+                {
+                    Logger.Info("Continued: " + properties[continuedStringPropertyName]);
+                    continuedStringPropertyName = null;
+                }
+                return true;
+            }
+            return false;
+        }
+
+        private void AddProperty(int separatorIndex)
+        {
+            string propertyName = line.Substring(0, separatorIndex).Trim();
+            string propertyValue = line.Substring(separatorIndex + 1).Trim();
+            if (ContainsMultiLineStart(propertyValue))
+                continuedStringPropertyName = propertyName;
+            propertyValue = EliminateLiteralMark(propertyValue);
+            propertyValue = EliminateColorCode(propertyValue);
+            properties.Add(propertyName, propertyValue);
+        }
+
+        private void ReadToNextModsCategory(StreamReader streamReader)
+        {
+            string line;
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                line = line.Trim();
+                if (ExtractCategoryName(line) == "mods")
+                    break;
+            }
+        }
+
+        private static string ExtractCategoryName(string line)
+        {
+            if (line.StartsWith("[["))
+            {
+                int endBracketIndex = line.IndexOf("]]");
+                if (endBracketIndex == -1)
+                    return null;
+                return line.Substring(2, endBracketIndex - 2);
+            }
+            return null;
+        }
+
+        private static bool ContainsMultiLineStart(string line)
+        {
+            return line.StartsWith("\"\"\"") || line.StartsWith("\'\'\'");
+        }
+
+        private static bool ContainsMultiLineEnd(string line)
+        {
+            return line.EndsWith("\"\"\"") || line.EndsWith("\'\'\'");
+        }
+
+        private static string EliminateLiteralMark(string value)
+        {
+            if (value.Contains("#"))
+                value = value.Substring(0, value.IndexOf('#'));
+            value = value.Trim('\"', '\'', ' ', '\t');
+            return value;
+        }
+
+        private static string EliminateColorCode(string value)
+        {
+            int idx;
+            while((idx = value.IndexOf('§')) != -1)
+                value = value.Remove(idx, 2);
+            return value;
+        }
+    }
+}
